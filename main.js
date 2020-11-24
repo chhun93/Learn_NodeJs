13e2233d1475661a8909f5474f1c93ad18ca2630
chhun93 <chhun3830@gmail.com>
2020-11-24 21:39:14 +0900

    upload file list in directory

--- main.js
+++ main.js
@@ -5,38 +5,67 @@ var url = require("url");
 var app = http.createServer(function (request, response) {
   var requestUrl = request.url;
   var queryData = url.parse(requestUrl, true).query;
-  var title = queryData.id;
-  console.log(queryData.id);
+  var pathname = url.parse(requestUrl, true).pathname;
 
-  if (requestUrl == "/") {
-    title = "Welcome";
-  }
-  if (requestUrl == "/favicon.ico") {
-    return response.writeHead(404);
-  }
-  response.writeHead(200);
+  if (pathname === "/") {
+    if (queryData.id === undefined) {
+      fs.readdir("./data", function (error, fileList) {
+        var title = "WELCOME";
+        var description = "HELLO NODE JS";
 
-  fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
-    var template = `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    <ol>
-      <li><a href="/?id=HTML">HTML</a></li>
-      <li><a href="/?id=CSS">CSS</a></li>
-      <li><a href="/?id=JavaScript">JavaScript</a></li>
-    </ol>
-    <h2>${title}</h2>
-    <p>${description}</p>
-  </body>
-  </html>
-  `;
-    response.end(template);
-  });
+        var list = "<ol>";
+        for (const i in fileList)
+          list += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+        list += "</ol>";
+
+        var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+        fs.readdir("./data", function (error, fileList) {
+          var title = queryData.id;
+
+          var list = "<ol>";
+          for (const i in fileList)
+            list += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+          list += "</ol>";
+
+          var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          ${list}
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    }
+  }
 });
-app.listen(80);
+app.listen(3000);
