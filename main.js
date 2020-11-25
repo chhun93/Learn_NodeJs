48f354099d1d62e6cc8f5fe9d62c46f1e548ffde
chhun93 <chhun3830@gmail.com>
2020-11-25 21:53:40 +0900

    정리

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 var http = require("http");
 var fs = require("fs");
 var url = require("url");
+const { brotliDecompressSync } = require("zlib");
+
+function templateHtml(title, list, body) {
+  return `
+<!doctype html>
+<html>
+<head>
+  <title>WEB1 - ${title}</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="/">WEB</a></h1>
+  ${list}
+  ${body}
+</body>
+</html>
+`;
+}
+
+function templateList(fileList) {
+  var list = "<ol>";
+  for (var i in fileList)
+    list += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
+  list += "</ol>";
+
+  return list;
+}
 
 var app = http.createServer(function (request, response) {
   var requestUrl = request.url;
@@ -12,58 +39,32 @@ var app = http.createServer(function (request, response) {
       fs.readdir("./data", function (error, fileList) {
         var title = "WELCOME";
         var description = "HELLO NODE JS";
-
-        var list = "<ol>";
-        for (const i in fileList)
-          list += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-        list += "</ol>";
-
-        var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        ${list}
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
+        var list = templateList(fileList);
+        var template = templateHtml(
+          title,
+          list,
+          `<h2>${title}</h2>${description}`
+        );
         response.writeHead(200);
         response.end(template);
       });
     } else {
-      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
-        fs.readdir("./data", function (error, fileList) {
-          var title = queryData.id;
-
-          var list = "<ol>";
-          for (const i in fileList)
-            list += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-          list += "</ol>";
-
-          var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-          response.writeHead(200);
-          response.end(template);
-        });
+      fs.readdir("./data", function (error, fileList) {
+        fs.readFile(
+          `data/${queryData.id}`,
+          "utf8",
+          function (err, description) {
+            var title = queryData.id;
+            var list = templateList(fileList);
+            var template = templateHtml(
+              title,
+              list,
+              `<h2>${title}</h2>${description}`
+            );
+            response.writeHead(200);
+            response.end(template);
+          }
+        );
       });
     }
   }
